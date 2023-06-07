5f84d1a2ec974b3aa325bd811ac65bc573f572d1
mgarcas <miggarcia90@gmail.com>
2023-06-07 18:11:59 -0400

    test commit from raspi

--- raspi_flask_vestal.py
+++ raspi_flask_vestal.py
@@ -14,4 +14,6 @@ def data_temp():
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.7', port=5000, debug=True)
+    #app.run(host='192.168.0.7', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
+
