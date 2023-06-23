c7c5c3828be6eafdd468d4e291964a9fd716618a
mgarcas <miggarcia90@gmail.com>
2023-06-23 21:33:57 +0200

    reverse changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-import flask_sqlalchemy as fs
+from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
@@ -9,7 +9,7 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'database', 'instance', 'raspiFlaskVestal.db')
 
-db = fs.SQLAlchemy()
+db = SQLAlchemy()
 
 
 # Initialize SQLAlchemy with the Flask app
