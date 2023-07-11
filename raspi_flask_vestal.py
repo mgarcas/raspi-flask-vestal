e357e0d3ffcec6edc83e49e3d082ecca32e2f729
mgarcas <miggarcia90@gmail.com>
2023-07-11 00:11:41 -0400

    new port to run app

--- raspi_flask_vestal.py
+++ raspi_flask_vestal.py
@@ -3,5 +3,5 @@ from app import app
 if __name__ == '__main__':
     # app.run(host='192.168.0.7', port=5000, debug=True)
     # app.run()
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
